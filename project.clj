db0d605c80c8c72167d6015bcd8c68b8bedf58b5
Heather Moore <moore.hj@gmail.com>
2016-01-18 14:49:40 -0800

    add fb sdk cljs

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
+                 [netpyoung/fb-sdk-cljs "0.1.1"]
                  [hiccup "1.0.5"]]
-
   :plugins [[lein-ring "0.9.7"]
             [lein-environ "1.0.1"]]
   :ring {:handler brunchlotto.core/app}
